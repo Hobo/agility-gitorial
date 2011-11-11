7807c9f8a87dc39a7303a40440e7d28db9f4b772
Bryan Larsen <bryan@larsen.st>
2011-11-11 15:53:15 -0500

    trivial-unit-test
    Let us write a simple test to verify our factory works:
    
    SHOW_PATCH
    
    now type
    
        $ rake test:units
    
    to verify that your tests run.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
-    assert true
+    @user = create :user
+    assert_equal "Test User", @user.name
   end
 end
